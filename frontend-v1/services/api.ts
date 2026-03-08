3bd21a94853db428ee843a4a442c8ad00a5ca93a
omarwaness <160738773+omarwaness@users.noreply.github.com>
2026-03-08 17:43:45 +0100

    Change API base URL to production endpoint

--- frontend-v1/services/api.ts
+++ frontend-v1/services/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // Create Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://medpulse-lu6x.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
